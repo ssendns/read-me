d4c9bfbfd76a542c192dfcf87771f568f3acd987
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    font imported

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        typewriter: ['"Courier Prime"', "monospace"],
+      },
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
