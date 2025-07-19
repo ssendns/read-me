3df6952d061756cc352ab0fe3e20308c086b67d0
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    created link to search page

--- frontend/src/RouterWrapper.jsx
+++ frontend/src/RouterWrapper.jsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import SearchPage from "./pages/SearchPage";
 
 export default function RouterWrapper() {
-  const routes = [{ path: "/", element: <LandingPage /> }];
+  const routes = [
+    { path: "/", element: <LandingPage /> },
+    {
+      path: "/search",
+      element: <SearchPage />,
+    },
+  ];
   const router = createBrowserRouter(routes);
   return <RouterProvider router={router} />;
 }
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
   return (
     <section className="text-center py-16">
       <h2 className="text-4xl font-bold mb-4">nothing beats a good book</h2>
-      <p className="text-lg text-gray-700">explore now</p>
+      <Link to="/search" className="text-lg text-gray-700 hover:underline">
+        explore now
+      </Link>
     </section>
   );
 }
