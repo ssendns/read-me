d71f2c9b137e0e18b2b3682509c26fcb5ab88392
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    made title and author fields have fixed amount of lines

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -12,17 +12,29 @@ export default function BookCard({
   return (
     <Link
       to={linkPath}
-      className="bg-bg text-text rounded-lg shadow-card p-block flex flex-col items-center justify-center w-60 hover:shadow-md transition font-typewriter"
+      className="bg-bg text-text rounded-lg shadow-card px-4 pt-4 pb-2 flex flex-col items-center w-44 h-75 hover:shadow-md transition font-typewriter"
     >
       <img
         src={coverUrl}
         alt={title}
-        className="w-32 h-48 object-cover rounded-md mb-4"
+        className="w-28 h-40 object-cover rounded mb-3"
       />
-      <h2 className="text-center text-lg font-semibold text-primary">
+
+      <h2
+        className="text-center text-[1rem] font-semibold text-primary leading-tight mb-1"
+        style={{
+          display: "-webkit-box",
+          WebkitLineClamp: 2,
+          WebkitBoxOrient: "vertical",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
+          height: "2.5em",
+        }}
+      >
         {title}
       </h2>
-      <p className="text-sm text-muted text-center">{author}</p>
+
+      <p className="text-sm text-muted text-center truncate w-full">{author}</p>
     </Link>
   );
 }
