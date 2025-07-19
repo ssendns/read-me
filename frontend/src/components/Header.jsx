b96728ab127d8d2a092b2e574229167638d7c5bc
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    changed links in the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-gray-50 text-black px-6 py-4 shadow-md flex justify-between items-center">
-      <h1 className="text-2xl font-bold">read-me</h1>
+      <Link to="/" className="hover:underline font-bold text-xl">
+        read-me
+      </Link>
       <nav className="space-x-4">
-        <Link to="/#" className="hover:underline">
-          home
-        </Link>
         <Link to="/search" className="hover:underline">
           search
         </Link>
-        <Link to="/#" className="hover:underline">
+        <Link to="/search" className="hover:underline">
           my library
+        </Link>
+        <Link to="/#" className="hover:underline">
+          account
         </Link>
       </nav>
     </header>
