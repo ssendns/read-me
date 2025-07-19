2b909842918cc9ab74cb1598fac4b6048d77d62f
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    removed strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import RouterWrapper from "./RouterWrapper";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterWrapper />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<RouterWrapper />);
