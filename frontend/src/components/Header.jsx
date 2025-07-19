86617f9500653338392308d9ffbf433539b6b8f0
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    added username display to the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,40 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const username = localStorage.getItem("username");
+  const isLoggedIn = Boolean(username);
+
   return (
     <header className="bg-bg text-text px-layoutX py-layoutY border-b border-color-border flex justify-between items-center font-typewriter">
-      <Link to="/" className="hover:underline font-bold text-xl">
-        read-me
-      </Link>
+      <nav className="space-x-4">
+        <Link to="/" className="hover:underline font-bold text-xl">
+          read-me
+        </Link>
+        <Link to="/#" className="hover:underline">
+          about
+        </Link>
+      </nav>
       <nav className="space-x-4">
         <Link to="/search" className="hover:underline">
           search
         </Link>
-        <Link to="/search" className="hover:underline">
-          my library
-        </Link>
-        <Link to="/#" className="hover:underline">
-          account
-        </Link>
+        {isLoggedIn ? (
+          <>
+            <Link to="/#" className="hover:underline">
+              my library
+            </Link>
+            <span className="hover:underline">@{username}</span>
+          </>
+        ) : (
+          <>
+            <Link to="/log-in" className="hover:underline">
+              log in
+            </Link>
+            <Link to="/sign-up" className="hover:underline">
+              sign up
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
