494fe1547ad41bc5dbf6562e653779dcf8c063cc
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-07-19 18:04:09 +0300

    created link to search page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="bg-gray-50 text-black px-6 py-4 shadow-md flex justify-between items-center">
       <h1 className="text-2xl font-bold">read-me</h1>
       <nav className="space-x-4">
-        <a href="#" className="hover:underline">
+        <Link to="/#" className="hover:underline">
           home
-        </a>
-        <a href="#" className="hover:underline">
+        </Link>
+        <Link to="/search" className="hover:underline">
+          search
+        </Link>
+        <Link to="/#" className="hover:underline">
           my library
-        </a>
-        <a href="#" className="hover:underline">
-          account
-        </a>
+        </Link>
       </nav>
     </header>
   );
